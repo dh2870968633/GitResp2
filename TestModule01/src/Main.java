c6e5996d3f2a2acdca2b937cb33681e02f366126
dh <2870968633@qq.com>
2024-06-27 10:43:57 +0800

    第一次使用idea进行集成Git， 进行数据的提交。

---
+++ TestModule01/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author dh
+ * @date 2024/6/27
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ TestModule01/src/com/msb/Student.java
@@ -0,0 +1,8 @@
+package com.msb;
+
+/**
+ * @author dh
+ * @date 2024/6/27
+ */
+public class Student {
+}
